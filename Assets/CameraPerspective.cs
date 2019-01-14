07a18689aca7c955d9a6d4e9ad57bdfde460c3b4
MinerMax555 <minermax555@gmail.com>
2019-01-14 20:33:12 +0100

    added .gitignore and all untracked files

---
+++ Assets/CameraPerspective.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPerspectiveScript : MonoBehaviour
+{
+
+	public Camera cam;
+
+	int lastchanged = -60;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.A))
+		{
+			if (Time.frameCount - lastchanged <= 60) return;
+			var ort = cam.orthographic;
+			cam.orthographic = !ort;
+			lastchanged = Time.frameCount;
+		}
+	}
+}
